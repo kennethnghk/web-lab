daddfa1e4d36cd715bd7c6145771b47b21131208
Kenneth Ng <ng.tszcheong@gmail.com>
2017-12-31 20:57:13 +0800

    #2 Install styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
-const page = () => <div>Welcome to next.js!</div>;
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const page = () => (
+  <div>
+    <Title>This is title</Title>
+    <div>Welcome to next.js!</div>
+  </div>
+);
 
 export default page;
