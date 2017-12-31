aa326e45af87383558958151abbe2a621e39378d
Kenneth Ng <ng.tszcheong@gmail.com>
2017-12-31 21:28:02 +0800

    #2 Include BBC gel-grid

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import stylesheet from "styles/global.scss";
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -9,8 +10,18 @@ const Title = styled.h1`
 
 const page = () => (
   <div>
+    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+
     <Title>This is title</Title>
     <div>Welcome to next.js!</div>
+    <div className="gel-wrap">
+      <div className="gel-layout">
+        <div className="gel-layout__item gel-1/2 gel-1/4@m">a</div>
+        <div className="gel-layout__item gel-1/2 gel-1/4@m">b</div>
+        <div className="gel-layout__item gel-1/2 gel-1/4@m">c</div>
+        <div className="gel-layout__item gel-1/2 gel-1/4@m">d</div>
+      </div>
+    </div>
   </div>
 );
 
