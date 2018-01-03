650755dfdc0a2e1bd8791b67fcc5bdf5cb3ada10
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-03 17:47:50 +0800

    #2 Convert to layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,14 @@
 import React from "react";
-import styled from "styled-components";
-import ThemeProvider from "react-toolbox/lib/ThemeProvider";
-import Button from "react-toolbox/lib/button/Button";
-import AppBar from "react-toolbox/lib/app_bar/AppBar";
-import theme from "styles/theme";
-import Head from "next/head";
-
-import css from "styles/theme.css";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+import Layout from "../components/Layout";
+import List from "react-toolbox/lib/list/List";
+import ListItem from "react-toolbox/lib/list/ListItem";
 
 const page = () => (
-  <div>
-    <Title>This is title</Title>
-    <div>Welcome to next.js!</div>
-    <Head>
-      <style dangerouslySetInnerHTML={{ __html: css }} />
-    </Head>
-    <ThemeProvider theme={theme}>
-      <AppBar>
-        <Button raised primary>
-          Hello
-        </Button>
-      </AppBar>
-    </ThemeProvider>
-  </div>
+  <Layout>
+    <List selectable ripple>
+      <ListItem caption="Jonathan 'Jon' Osterman" />
+    </List>
+  </Layout>
 );
 
 export default page;
