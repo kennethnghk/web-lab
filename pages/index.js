2a73820cfcf816e8f6d0eeec454ef1f569b9cc1f
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-03 18:54:44 +0800

    #2 Create simple list

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { List, ListItem } from "material-ui/List";
+import ActionInfo from "material-ui/svg-icons/action/info";
 
 const Index = () => (
   <Layout>
-    <div>Welcome to next.js!</div>
+    <List>
+      <ListItem primaryText="Item 1" rightIcon={<ActionInfo />} />
+      <ListItem primaryText="Item 2" rightIcon={<ActionInfo />} />
+      <ListItem primaryText="Item 3" rightIcon={<ActionInfo />} />
+      <ListItem primaryText="Item 4" rightIcon={<ActionInfo />} />
+    </List>
   </Layout>
 );
 
