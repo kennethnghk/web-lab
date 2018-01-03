a9a74f8fec2f3330c4643d1ad0fbf6530828d1c9
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-03 14:39:01 +0800

    #2 Integrate react-toolbox according to https://github.com/zeit/next.js/tree/master/examples/with-react-toolbox

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import ThemeProvider from "react-toolbox/lib/ThemeProvider";
+import Button from "react-toolbox/lib/button/Button";
+import AppBar from "react-toolbox/lib/app_bar/AppBar";
+import theme from "../static/theme";
+import Head from "next/head";
 
 const Title = styled.h1`
   font-size: 1.5em;
@@ -11,6 +16,16 @@ const page = () => (
   <div>
     <Title>This is title</Title>
     <div>Welcome to next.js!</div>
+    <Head>
+      <link href="/static/theme.css" rel="stylesheet" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <AppBar>
+        <Button raised primary>
+          Hello
+        </Button>
+      </AppBar>
+    </ThemeProvider>
   </div>
 );
 
