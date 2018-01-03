543e79fa548545bb23ad53917c5e6c0c9745a513
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-03 19:40:06 +0800

    #4 List items from Instagram feed

---
+++ pages/feed.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "isomorphic-unfetch";
+import { Card, CardHeader, CardMedia, CardText } from "material-ui/Card";
+import Layout from "../components/Layout";
+
+const FEED_URL = "https://www.instagram.com/euro2020/?__a=1";
+
+const Feed = props => (
+  <Layout>
+    {props.items.map((item, index) => (
+      <Card style={{ margin: "0 0 10px 0" }} key={index}>
+        <CardHeader
+          title={props.user.username}
+          avatar={props.user.profile_pic_url}
+        />
+        <CardMedia>
+          <img src={item.thumbnail_src} alt="" />
+        </CardMedia>
+        <CardText>{item.caption}</CardText>
+      </Card>
+    ))}
+  </Layout>
+);
+
+Feed.getInitialProps = async () => {
+  const res = await fetch(FEED_URL);
+  const json = await res.json();
+  return {
+    user: json.user,
+    items: json.user.media.nodes
+  };
+};
+
+Feed.propTypes = {
+  user: PropTypes.object,
+  items: PropTypes.array
+};
+
+export default Feed;
