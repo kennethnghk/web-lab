1bde326738f4e6a105334a9b5d09ec545588f434
Kenneth Ng <ng.tszcheong@gmail.com>
2018-01-03 18:22:43 +0800

    #2 Add icon

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Layout from "../components/Layout";
 import List from "react-toolbox/lib/list/List";
 import ListItem from "react-toolbox/lib/list/ListItem";
+import Icon from "react-icons/lib/md/label";
 
-const page = () => (
+const Page = () => (
   <Layout>
     <List selectable ripple>
-      <ListItem caption="Jonathan 'Jon' Osterman" />
+      <ListItem caption="Jonathan 'Jon' Osterman" leftIcon={<Icon />} />
     </List>
   </Layout>
 );
 
-export default page;
+export default Page;
